f33c93c3cc4276e0f0fe65b001a2157938981c29
Carlos Fins <clockwerkz@gmail.com>
2019-05-25 17:03:16 -0700

    blocked in pagination

--- src/App.js
+++ src/App.js
@@ -5,25 +5,39 @@ const meteoriteAPI = 'https://data.nasa.gov/resource/gh4g-9sfh.json';
 function App() {
   
   const [data, setData] = useState([]);
+  const [dataSlice, setDataSlice] = useState([]);
+  const [pageSize, setPageSize] = useState(20);
+  const [pagePosition, setPagePosition] = useState(0);
 
   useEffect(()=> {
     fetch(meteoriteAPI)
       .then(res => res.json())
       .then(jsonData => setData(jsonData))
       .catch(err => console.error('Problem with data:', err));
-  }, [])
+  }, []);
 
+  useEffect(()=> {
+    if (data) {
+      setDataSlice(data.slice(pagePosition, pagePosition+pageSize));
+    }
+  }, [data, pagePosition, pageSize])
+
+
+  const updatePagination =() => {
+    setPagePosition(pagePosition+pageSize);
+  }
 
   return (
     <div>
       <h1>Meteorite Data</h1>
       <ul>
-        { data && data.map(meteor => (
+        { dataSlice && dataSlice.map(meteor => (
          <li key={meteor.name}>
           {meteor.name}
         </li> 
         )) }
       </ul>
+      <button onClick={updatePagination}>Next Page</button>
     </div>
   );
 }
