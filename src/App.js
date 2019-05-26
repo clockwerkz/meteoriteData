6a953730d743799a63873fd99584639c190f8454
Carlos Fins <clockwerkz@gmail.com>
2019-05-25 17:22:59 -0700

    adds table format, displays more data

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React, { useState, useEffect } from 'react';
-
+import './app.css';
 const meteoriteAPI = 'https://data.nasa.gov/resource/gh4g-9sfh.json';
 
 function App() {
   
   const [data, setData] = useState([]);
+  const [defaultData, setDefaultData] = useState([]);
   const [dataSlice, setDataSlice] = useState([]);
   const [pageSize, setPageSize] = useState(20);
   const [pagePosition, setPagePosition] = useState(0);
@@ -17,27 +18,46 @@ function App() {
   }, []);
 
   useEffect(()=> {
+    setDefaultData(data);
+  }, [data])
+
+  useEffect(()=> {
     if (data) {
-      setDataSlice(data.slice(pagePosition, pagePosition+pageSize));
+      setDataSlice(defaultData.slice(pagePosition, pagePosition+pageSize));
     }
-  }, [data, pagePosition, pageSize])
+  }, [defaultData, pagePosition, pageSize])
 
 
-  const updatePagination =() => {
-    setPagePosition(pagePosition+pageSize);
+  const advancePagination =() => {
+    if (pagePosition < data.length){
+      setPagePosition(pagePosition+pageSize);
+    }
+  }
+
+  const reversePagination = () => {
+    if (pagePosition > 0) {
+      setPagePosition(pagePosition-pageSize);
+    }
   }
 
   return (
     <div>
       <h1>Meteorite Data</h1>
-      <ul>
+      <table>
         { dataSlice && dataSlice.map(meteor => (
-         <li key={meteor.name}>
-          {meteor.name}
-        </li> 
+         <tr key={meteor.id}>
+          <td>{meteor.name}</td>
+          <td>{meteor.recclass}</td>
+          <td>{meteor.mass}</td>
+          <td>{meteor.fall}</td>
+          <td>{Date(meteor.year)}</td>
+        </tr> 
         )) }
-      </ul>
-      <button onClick={updatePagination}>Next Page</button>
+      </table>
+      <div>
+        <button onClick={reversePagination}>Previous Page</button>
+        <button onClick={advancePagination}>Next Page</button>
+      </div>
     </div>
   );
 }
