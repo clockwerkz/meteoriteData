3e6cbc7268294f98b340e9898367e149f5501def
Carlos Fins <clockwerkz@gmail.com>
2019-05-27 15:08:43 -0700

    moves search bar/button into their own div

--- src/Components/SearchBar.js
+++ src/Components/SearchBar.js
@@ -8,8 +8,10 @@ const SearchBar = () => {
     const [search, setSearch] = useState(searchString);
     return ( 
        <div>
-            <input type='text' value={search} onChange={(e)=> setSearch(e.target.value)} />
-            <button onClick={()=> updateSearch(search)}>Search</button>
+            <div>
+                <input type='text' value={search} onChange={(e)=> setSearch(e.target.value)} />
+                <button onClick={()=> updateSearch(search)}>Search</button>
+            </div>
             <label htmlFor='SearchType'>Search Options:</label>
             <select name='SearchType' onChange={(e)=> setSearchFilter(e.target.value)}>
                 <option value="startsWith">Starts With</option>
