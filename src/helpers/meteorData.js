686202e5a1a3c28827dbb334c047f2b716183155
clockwerkz <clockwerkz@gmail.com>
2019-05-27 22:55:03 -0700

    fixed the query so that it matches to all lower

--- src/helpers/meteorData.js
+++ src/helpers/meteorData.js
@@ -15,30 +15,27 @@ const fetchInitialData = (page, offset, cb) => {
 
 
 const queryDataStartsWith = (page, offset, cb, searchString) => {
-    searchString = searchString[0].toUpperCase() + searchString.slice(1).toLowerCase();
-      consumer.query()
-        .withDataset('gh4g-9sfh')
-        .order('name ASC')
-        .where("name like '"+searchString+"%'")
-        .limit(page)
-        .offset(offset)
-        .getRows()
-        .on('success', (data)=> cb(data))
-        .on('error', (error)=> console.error(error));
+  consumer.query()
+    .withDataset('gh4g-9sfh')
+    .order('name ASC')
+    .where(`lower(name) like '${searchString.toLowerCase().trim()}%'`)
+    .limit(page)
+    .offset(offset)
+    .getRows()
+    .on('success', (data)=> cb(data))
+    .on('error', (error)=> console.error(error));
 }
 
 const queryDataContains = (page, offset, cb, searchString) => {
-    const searchStringUpper = searchString[0].toUpperCase() + searchString.slice(1).toLowerCase();
-    searchString = searchString.toLowerCase();
-      consumer.query()
-        .withDataset('gh4g-9sfh')
-        .order('name ASC')
-        .where("name like '%"+searchString+"%' OR name like '%"+searchStringUpper+"%'")
-        .limit(page)
-        .offset(offset)
-        .getRows()
-        .on('success', (data)=> cb(data))
-        .on('error', (error)=> console.error(error));
+  consumer.query()
+    .withDataset('gh4g-9sfh')
+    .order('name ASC')
+    .where("lower(name) like '%"+encodeURIComponent(searchString.toLowerCase().trim())+"%'")
+    .limit(page)
+    .offset(offset)
+    .getRows()
+    .on('success', (data)=> cb(data))
+    .on('error', (error)=> console.error(error));
 }
 
 export { fetchInitialData , queryDataStartsWith, queryDataContains }
